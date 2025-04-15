f1f7935414a062b88fd74687b211de0b1c0b39d2
Thanapon Johdee <sern.dev@gmail.com>
2025-04-16 01:43:35 +0700

    Add user repository with CRUD operations

---
+++ internal/adaptor/repository/user_repository.go
@@ -0,0 +1,69 @@
+package repository
+
+import (
+	"github.com/yokeTH/gofiber-template/internal/adaptor/dto"
+	"github.com/yokeTH/gofiber-template/internal/domain"
+	"github.com/yokeTH/gofiber-template/pkg/apperror"
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *userRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (r *userRepository) GetUserByID(id string) (*domain.User, *apperror.AppError) {
+	var user domain.User
+
+	if err := r.db.Where("id = ?", id).First(&user).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, apperror.NotFoundError(err, "user not found")
+		}
+		return nil, apperror.InternalServerError(err, "failed to find user")
+	}
+	return &user, nil
+}
+
+func (r *userRepository) GetUserByProvider(provider, providerID string) (*domain.User, *apperror.AppError) {
+	var user domain.User
+
+	if err := r.db.Where("provider = ? AND provider_id = ?", provider, providerID).First(&user).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, apperror.NotFoundError(err, "user not found")
+		}
+		return nil, apperror.InternalServerError(err, "failed to find user")
+	}
+	return &user, nil
+}
+
+func (r *userRepository) CreateUser(user *domain.User) (*domain.User, *apperror.AppError) {
+	if err := r.db.Create(user).Error; err != nil {
+		return nil, apperror.InternalServerError(err, "failed to create user")
+	}
+	return user, nil
+}
+
+func (r *userRepository) UpdateUserInfo(userID string, updatedData dto.UpdateUserRequest) *apperror.AppError {
+	if err := r.db.
+		Model(&domain.User{}).
+		Where("id = ?", userID).
+		Updates(updatedData).Error; err != nil {
+		return apperror.InternalServerError(err, "failed to update user info")
+	}
+	return nil
+}
+
+func (r *userRepository) SetIsOnline(userID string, isOnline bool) *apperror.AppError {
+	if err := r.db.
+		Model(&domain.User{}).
+		Where("id = ?", userID).
+		Updates(domain.User{IsOnline: isOnline}).Error; err != nil {
+		return apperror.InternalServerError(err, "failed to update user info")
+	}
+	return nil
+}
